2b2431bf2a1514df4ca6c8657adc4a95dce975ed
GiovanaZanirato <84473403+GiovanaZanirato@users.noreply.github.com>
2021-06-02 15:55:40 -0300

    Add files via upload

---
+++ Giovana_210601.py
@@ -0,0 +1,47 @@
+#question 2
+car=["jeep","bmw","ferrari","toyota","porche","fiat"]
+print(car)
+#question 3
+print(car[1])
+#question 4
+car.append("bicycles")
+#question 5
+print(car)
+#question 6
+car.sort()
+#question 7
+print(car)
+#question 8
+barbecue=("bicycle " " bmw")
+#question 9
+print(barbecue)
+#question 10
+cheddar="toyota"
+#question 11
+print(cheddar.upper())
+#question 12
+car.insert(2,"nova odessa")
+#question 13
+print(car)
+#question 14
+del car[1]
+#question 15
+print(car)
+#question 16
+popped_sander=car.pop()
+# question 17
+print(car)
+print(popped_sander)
+#question 18
+popped_fernanda=car.pop(0)
+#qeustion 19
+print(car)
+print(popped_fernanda)
+#question 20
+fruit=["melon","banana","apple"]
+#question 21
+print(fruit)
+#question 22
+fruit.sort()
+#question 23
+print(fruit)
